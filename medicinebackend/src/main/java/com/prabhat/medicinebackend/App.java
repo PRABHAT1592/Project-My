14877e51781aafebe4459aabadb9b3e8f40bf888
Prabhat Kumar <prabhat15.cusat@gmail.com>
2018-03-19 20:32:34 +0530

    setup done

---
+++ medicinebackend/src/main/java/com/prabhat/medicinebackend/App.java
@@ -0,0 +1,13 @@
+package com.prabhat.medicinebackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
